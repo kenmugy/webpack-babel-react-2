3a889671e7e84e7a149a3b79f67d30b44a83536c
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-05-24 20:23:13 +0300

    Add materialize-css framework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import 'materialize-css/dist/css/materialize.min.css';
 
 render(<App />, document.getElementById('root'));
 module.hot.accept();
