43952fc639d73108173a665079250ac6a217ff04
kenmugy <42069371+kenmugy@users.noreply.github.com>
2020-05-24 20:21:25 +0300

    Link App component to index file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 
-render('hello', document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 module.hot.accept();
